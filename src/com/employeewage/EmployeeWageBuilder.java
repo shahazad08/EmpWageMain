d71dd1556631bdc5453800fdde7615a575bb07ee
shahazad08 <sk.shahazad@gmail.com>
2022-06-17 09:01:25 +0530

    Added: Employee Attedance FUnctionality added

--- src/com/employeewage/EmployeeWageBuilder.java
+++ src/com/employeewage/EmployeeWageBuilder.java
@@ -1,7 +1,18 @@
 package com.employeewage;
 
+import java.util.Random;
+
 public class EmployeeWageBuilder {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Builder");
+        int IS_FULL_TIME=1;
+        Random random=new Random();
+        int empCheck=random.nextInt(2);
+        if(empCheck==IS_FULL_TIME) {
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 }
