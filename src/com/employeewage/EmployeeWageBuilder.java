cbcacb9e849ef69de0cc580b079ae76222efcfac
shahazad08 <sk.shahazad@gmail.com>
2022-06-17 09:13:19 +0530

    Added: Full time functionality added

--- src/com/employeewage/EmployeeWageBuilder.java
+++ src/com/employeewage/EmployeeWageBuilder.java
@@ -6,13 +6,19 @@ public class EmployeeWageBuilder {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Builder");
         int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR=20;
+        int empHours=0;
+        int empWage=0;
         Random random=new Random();
         int empCheck=random.nextInt(2);
         if(empCheck==IS_FULL_TIME) {
             System.out.println("Employee is present");
+            empHours=8;
         }
         else {
             System.out.println("Employee is absent");
         }
+        empWage=empHours*EMP_RATE_PER_HOUR;
+        System.out.println("The daily wage of EMployee is "+ empWage);
     }
 }
