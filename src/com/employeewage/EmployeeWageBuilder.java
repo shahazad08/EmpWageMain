2fb0cf8097dc957ec340741bd52084619137ba2e
shahazad08 <sk.shahazad@gmail.com>
2022-06-18 10:28:37 +0530

    Modified: if else change to switch case

--- src/com/employeewage/EmployeeWageBuilder.java
+++ src/com/employeewage/EmployeeWageBuilder.java
@@ -3,27 +3,30 @@ package com.employeewage;
 import java.util.Random;
 
 public class EmployeeWageBuilder {
+
+    public static final int IS_FULL_TIME=1;
+    public static final int IS_PART_TIME=2;
+    public static final int EMP_RATE_PER_HOUR=20;
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Builder");
-        int IS_FULL_TIME=1;
-        int IS_PART_TIME=2;
-        int EMP_RATE_PER_HOUR=20;
         int empHours=0;
         int empWage=0;
         Random random=new Random();
         int empCheck=random.nextInt(3);
-        if(empCheck==IS_FULL_TIME) {
-            System.out.println("Employee is present");
-            empHours=8;
+        switch(empCheck) {
+            case IS_FULL_TIME:
+                System.out.println("Employee is present");
+                empHours=8;
+                break;
+            case IS_PART_TIME:
+                empHours=4;
+                System.out.println("Employee is Half Day");
+                break;
+            default:
+                empHours=0;
         }
-        else if(IS_PART_TIME==empCheck) {
-            empHours=4;
-            System.out.println("Employee is Half Day");
-        }
-        else {
-            empHours=0;
-        }
+
         empWage=empHours*EMP_RATE_PER_HOUR;
-        System.out.println("The daily wage of EMployee is "+ empWage);
+        System.out.println("The daily wage of Employee's is"+ empWage);
     }
 }
